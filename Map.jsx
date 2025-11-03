ef1b2592110deeddfc5a21fc0e544a7ea042fcf4
Sharanya-sys <thantrysharanya@gmail.com>
2025-11-03 21:36:23 +0530

    Add files via upload

---
+++ Map.jsx
@@ -0,0 +1,3 @@
+export default function Map() {
+  return <div className="p-4">Museum Map coming soon…</div>;
+}
---
+++ Tour.jsx
@@ -0,0 +1,3 @@
+export default function Tour() {
+  return <div className="p-4">Guided Tour coming soon…</div>;
+}
